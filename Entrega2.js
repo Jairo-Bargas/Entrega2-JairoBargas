7c77c10b1187e98ea8a8fc8a3b992df1a39a78ba
jfbargas <jfacundobargas2@gmail.com>
2024-08-27 23:32:19 -0300

    Nueva modificación

--- Entrega2.js
+++ Entrega2.js
@@ -33,6 +33,88 @@ const productos = [
     {serie: 9, nombreProducto: "Tractor Serie 9", unidadesCotizables: 90},
 ]
 
+const serviciosOfrecidos = [
+    {id: 1, servicio: "Lavado de maquinaria", price: 1000},
+    {id: 2, servicio: "Servicio de mantenimiento", price: 2000},
+    {id: 3, servicio: "Visita a campo", price: 500},
+    {id: 4, servicio: "Paquetes de monitoreo", price: 4000}
+]
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+const seriesOfrecidas = JSON.parse(localStorage.getItem("seriesOfrecidas")) || []
+
+
+const nuevoProducto = ({serie, nombreProducto, unidadesCotizables})=>{
+    const productoAgregado = new ProductosNuevos(serie, nombreProducto, unidadesCotizables)
+    seriesOfrecidas.push(productoAgregado)
+    localStorage.setItem(`seriesOfrecidas`, JSON.stringify(seriesOfrecidas))
+}
+/* FUNCIÓN PARA CREAR PRODUCTOS */ 
+function renderizarProductos() {
+    const productosDiv = document.getElementById('productos');
+    productosDiv.innerHTML = ''; 
+    const usuario = JSON.parse(localStorage.getItem(`usuario`));
+    if (!usuario) return;
+    const maquina = productos.find(maq => maq.nombreProducto === usuario.duenioDe)
+
+    serviciosOfrecidos.forEach(producto => {
+        const productoDiv = document.createElement('div');
+        productoDiv.innerHTML = `
+            <span>${producto.servicio} - $${producto.price * maquina.unidadesCotizables}</span>
+            <button onclick="agregarAlCarrito(${producto.id})">Añadir al Carrito</button>
+        `;
+        productosDiv.appendChild(productoDiv);
+    });
+}
+
+/* FUNCIÓN PARA AGREGAR AL CARRITO LOS SERVICIOS */
+
+function agregarAlCarrito(id){
+    const encontrarProducto = serviciosOfrecidos.find(p => p.id === id)
+    if(encontrarProducto){
+        const servicioEnCarrito = carrito.find(p=>p.id === id);
+        if(servicioEnCarrito){
+            servicioEnCarrito.quantity += 1;
+        } else {
+            carrito.push({...encontrarProducto, quantity: 1});
+        }
+        localStorage.setItem(`carrito`, JSON.stringify(carrito));
+        renderizarCarrito()
+    }
+}
+
+/* FUNCIÓN PARA CALCULAR EL TOTAL DEL CARRITO */
+function calcularTotalCarrito() {
+    const usuario = JSON.parse(localStorage.getItem(`usuario`));
+    const maquina2 = productos.find(maq => maq.nombreProducto === usuario.duenioDe)
+    return carrito.reduce((total, item) => {
+        return total + ((item.price * maquina2.unidadesCotizables)*item.quantity);
+    }, 0);
+}
+
+/* FUNCIÓN PARA RENDERIZAR EL CARRITO */
+
+const renderizarCarrito = ()=> {
+    const itemscarrito = document.getElementById('itemscarrito');
+    itemscarrito.innerHTML = '';
+
+    carrito.forEach(({servicio, id}) => {
+        let itemUl = document.createElement('li');
+        itemUl.innerHTML = `Servicio: ${servicio} \n <button onclick="eliminarCarrito(${id})">Borrar elección</button>`;
+        itemscarrito.appendChild(itemUl);
+    })          
+
+    const total = calcularTotalCarrito();
+    const totalDiv = document.getElementById('totalCarrito');
+    totalDiv.innerHTML = `Total: $${total}`;
+} 
+
+function eliminarCarrito(id) {
+    carrito = carrito.filter(item => item.id !== id);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    renderizarCarrito();
+}
+
 
 /* Función de ingreso que ejecuta las demás funciones */
 function login(){
@@ -65,10 +147,9 @@ function login(){
 
 });
 
-usuarioDentro()
 }
 
-/* Se crea la función que determina que tipo de usuario ingresó, es decir, que tipo de máquina posee para saber que precios se le ofrece */
+/* Se crea la función que determina que usuario ingresó */
 function usuarioDentro(){
     window.addEventListener(`DOMContentLoaded`, ()=>{
         const usuario = JSON.parse(localStorage.getItem(`usuario`));
@@ -77,31 +158,17 @@ function usuarioDentro(){
 
             bienvenida.textContent = `Bienvenido ${(usuario.nombre).toUpperCase()} ${(usuario.apellido).toUpperCase()}` 
         }
-        const maquina = productos.find(maq => maq.nombreProducto === usuario.duenioDe)
-        
-        const valor1 = document.getElementById(`precio1`)
-        const valor2 = document.getElementById("precio2")
-        const valor3 = document.getElementById(`precio3`)
-        const valor4 = document.getElementById("precio4")
-        if(valor1){
-            valor1.textContent = `Precio: $${(maquina.unidadesCotizables * 1000)}`
-        }
-        if(valor2){
-            valor2.textContent = `Precio: $${(maquina.unidadesCotizables * 2000)}`
-        }
-        if(valor3){
-            valor3.textContent = `Precio: $${(maquina.unidadesCotizables * 500)}`
-        }
-        if(valor4){
-            valor4.textContent = `Precio: $${(maquina.unidadesCotizables * 4000)}`
-        }
     })
 }
 
-login()
+const run=()=>{
+    login()
+    usuarioDentro()
+    renderizarCarrito()
+    renderizarProductos()
+}
 
 
-
-    
+run()
 
 
